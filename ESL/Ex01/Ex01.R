7234632d0b2f0e547c7edb5623c7141b9c8f72cc
raoanonymous <pramod_rao@outlook.com>
2019-10-27 17:50:47 +0100

    Finished ESL problem 8

--- ESL/Ex01/Ex01.R
+++ ESL/Ex01/Ex01.R
@@ -53,13 +53,12 @@ print(cor(ozone[1:4], method = "pearson" ))
 # c) The third variable, temperature is negatively correlated with wind.
 
 # Can you see them directly from the plot (visually)?
-library("PerformanceAnalytics")
+require("PerformanceAnalytics")
 chart.Correlation(ozone, histogram = FALSE)
 
 # 5---------------------------------------------------------------------------------
 print("Problem 4.5")
 rss <- function(true_value, predicted_value){
-    print("Calculating RSS ...")
     return(sum((true_value - predicted_value)^2))
 }
 print(rss(testset,testset+1))
@@ -69,8 +68,58 @@ print("Problem 4.6")
 train_data <- ozone[trainset,]
 test_data <- ozone[testset,]
 
-ozone_model <- lm(ozone ~ radiation+temperature+wind, data = train_data, graph = TRUE)
+ozone_model <- lm(ozone ~ radiation+temperature+wind, data = train_data)
 print(summary(ozone_model))
 pred_data <- predict.lm(ozone_model, test_data)
+
+test_lm_rss <- rss(test_data["ozone"], pred_data)
+test_lm_cor <- cor(test_data["ozone"], pred_data, method = "pearson")
+plot(test_data$ozone, pred_data, main = "Predicted and True Values Scatterplot",col = c("Blue"), pch=16)
+
+# 7---------------------------------------------------------------------------------
+print("Problem 4.7")
+require("FNN")
+knn_model <- function(train, test, n){
+   knn_pred <- knn.reg(train = train[2:4],test =  test[2:4],y = train[1],k = n)
+   knn_rss = rss(test$ozone, knn_pred$pred)
+   return(list(pred=knn_pred$pred, rss=knn_rss))
+}
+
+# Train Data
+i = 1
+train_knn_rss <- c()
+knn_predictions <- knn_model(train_data, train_data, i)
+train_knn_rss <- c(train_knn_rss, knn_predictions$rss)
+
+while(i<30){
+    knn_predictions <- knn_model(train_data, train_data, i+2)
+    train_knn_rss <- c(train_knn_rss, knn_predictions$rss)
+    i <- i+1
+}
+grid(1,1)
+plot(train_knn_rss, col="green", ylim = c(0,70000), main = "Train vs Pred RSS", pch=16, panel.first = grid())
+
+# Test Data
+i = 1
+test_knn_rss <- c()
+knn_predictions <- knn_model(train_data, test_data, i)
+test_knn_rss <- c(test_knn_rss, knn_predictions$rss)
+
+while(i<30){
+    knn_predictions <- knn_model(train_data, test_data, i+2)
+    test_knn_rss <- c(test_knn_rss, knn_predictions$rss) 
+    i <- i+1
+}
+points(test_knn_rss, col="blue", pch=16)
+
+# Assumptions in KNN:
+# https://saravananthirumuruganathan.wordpress.com/2010/05/17/a-detailed-introduction-to-k-nearest-neighbor-knn-algorithm/
+# 8---------------------------------------------------------------------------------
+print("Problem 4.8")
+# Chosen nearest neighbour = 6
+knn_predictions_best <- knn_model(train_data, test_data, 6)
+test_knn_cor_best <- cor(knn_predictions_best$pred, test_data$ozone, method = "pearson")
+cat("\n The RSS value of best kNN model: ", knn_predictions_best$rss,"\n Pearson Coeff for best kNN model is: ", test_knn_cor_best, "\n")
+cat("\n The RSS value of lm  model: ", test_lm_rss,"\n Pearson Coeff for lm model is: ", test_lm_cor[1], "\n")
 
 dev.off()
