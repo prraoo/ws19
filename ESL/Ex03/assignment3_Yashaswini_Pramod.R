fa196a5ca08bcbb52096b91384b7d09dc59879e2
raoanonymous <pramod_rao@outlook.com>
2019-11-27 09:37:08 +0100

    ESL-3, finished problem 3

--- ESL/Ex03/assignment3_Yashaswini_Pramod.R
+++ ESL/Ex03/assignment3_Yashaswini_Pramod.R
@@ -5,6 +5,7 @@
 #loading the package
 require(dplyr)
 require(MASS)
+require(ggplot2)
 # Dataset
 raw_data <- read.csv("phoneme.csv", header = TRUE,sep = ",")
 # 1---------------------------------------------------------------------------------
@@ -24,3 +25,27 @@ test.data <- dplyr::select(test.data,-c("index", "g"))
 print("-------------------------Problem 4.2-------------------------")
 train.model <- lda(train.data, grouping = train.responses)
 
+train.predictions <- predict(train.model, train.data)
+test.predictions <- predict(train.model, test.data)
+
+#accuracy
+print(mean(train.predictions$class==train.responses))
+print(mean(test.predictions$class==test.responses))
+
+# 3---------------------------------------------------------------------------------
+print("-------------------------Problem 4.3-------------------------")
+# projection of the training data onto the first two canonical coordinates of the LDA
+plot.df <- data.frame(train.predictions$x , "Outcome" = train.responses)
+ggplot(plot.df, aes(x = LD1, y = LD2, color = Outcome)) + geom_point()
+
+# 4---------------------------------------------------------------------------------
+print("-------------------------Problem 4.4-------------------------")
+
+train.data <- data[data$index == "train",]
+train.data_aa_ao <- train.data[data$g %in% c("aa", "ao"),]
+#train.responses <- data[data$index == "train",]$g
+#train.data <- dplyr::select(train.data,-c("index", "g"))
+
+#test.data <- data[data$index == "test",]
+#test.responses <- data[data$index == "test",]$g
+#test.data <- dplyr::select(test.data,-c("index", "g"))
--- ESL/Ex03/test.R
+++ ESL/Ex03/test.R
@@ -9,3 +9,16 @@ test.set <- subset(feature.df, !train)
 train.set <- subset(feature.df, train)
 train.responses <- subset(df, train)$g
 test.responses <- subset(df, !train)$g
+
+lda.model <- lda(train.set, grouping = train.responses)
+
+means <- colSums(lda.model$prior * lda.model$means)
+train.mod <- scale(train.set, center = means, scale = FALSE) %*% 
+    lda.model$scaling
+
+lda.prediction.train <- predict(lda.model, train.set)
+
+# visualize the features in the two LDA dimensions
+plot.df <- data.frame(train.mod, "Outcome" = train.responses)
+library(ggplot2)
+ggplot(plot.df, aes(x = LD1, y = LD2, color = Outcome)) + geom_point()
