ed909dddd1e9acd24664670973d94a07ffa26bc9
raoanonymous <pramod_rao@outlook.com>
2019-12-09 14:09:42 +0100

    ESL-04 Complete 4a and 4b

--- ESL/Ex04/assignment4_Pramod_Yashaswini.R
+++ ESL/Ex04/assignment4_Pramod_Yashaswini.R
@@ -1,17 +1,37 @@
+require(caret)
+#require()
+
 # load data
 raw_data <- read.table("prostate.txt")
 # 1---------------------------------------------------------------------------------
 print("-------------------------Problem 4.1-------------------------")
 # train-test split
 train <- raw_data[raw_data$train == "TRUE",]
-train.data <- (dplyr::select(train, -c("train", "lpsa")))
-train.data <- (train.data - colMeans(train.data))/(apply(train.data, 2, sd))
-train.labels <- (train$lpsa - mean(train$lpsa))/(sd(train$lpsa))
+train <- (dplyr::select(train, -c("train")))
+train.data <- data.frame(apply(train, 2, scale))
 
 test <- raw_data[raw_data$train == "FALSE",]
-test.data <- dplyr::select(test, -c("train", "lpsa"))
-test.data <- (test.data - colMeans(test))/apply(test.data, 2, sd)
-test.labels <- (test$lpsa - mean(test$lpsa))/sd(test$lpsa)
+test <- dplyr::select(test, -c("train"))
+test.data <- data.frame(apply(test, 2, scale))
 
 # 2---------------------------------------------------------------------------------
 print("-------------------------Problem 4.2-------------------------")
+# Linear Regression model
+#train.LOOCV <- lm(lpsa ~ .,data = train.data)
+# LOOCV
+train.control = trainControl(method = "LOOCV")
+train.LOOCV <- caret::train(lpsa ~ ., data= train.data, method = "lm", trControl=train.control)
+print("LOOCV Results: "); print(train.LOOCV$results)
+
+# 5-Fold
+train.control = trainControl(method = "cv", number = 5)
+train.5fold_CV <- caret::train(lpsa ~ ., data= train.data, method = "lm", trControl=train.control)
+print("5-Fold CV Results: "); print(train.5fold_CV$results)
+# 10-Fold
+train.control = trainControl(method = "cv", number = 10)
+train.10fold_CV <- caret::train(lpsa ~ ., data= train.data, method = "lm", trControl=train.control)
+print("10-Fold CV Results: "); print(train.10fold_CV$results)
+#Full train data
+train.no_CV <- caret::train(lpsa ~ ., data= train.data, method = "lm")
+print("No CV Results: "); print(train.no_CV$results)
+# Complete Train
